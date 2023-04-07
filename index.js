6b8610da4f66446d238ecbe16f643dfad9014b61
cMiD-F <cauet.cmd@gmail.com>
2023-04-07 10:33:34 -0300

    errorHandler

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const bodyParser = require("body-parser");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 dbConnect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.request(errorHandler);
+
 app.listen(PORT, () => {
     console.log(`Servidor está rodando na PORT ${PORT}`);
 });
