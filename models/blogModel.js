1755129025e87d7d7178f5e177b8007c3acfb95a
cMiD-F <cauet.cmd@gmail.com>
2023-04-27 18:28:39 -0300

    updates

---
+++ models/blogModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  numViews: {
+    type: Number,
+    required: true,
+  },
+  isLiked: {
+    type: Boolean,
+    default: false,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  dislikes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  image: {
+    type: String,
+    default:""
+  },
+  author: {
+    type: String,
+    default:"Admin",
+  }
+},{
+    toJSON: {
+        virtuals: true,
+    },
+    toObject: {
+        virtuals: true,
+    },
+    timestamps: true,
+});
+
+//Export the model
+module.exports = mongoose.model("Blog", blogSchema);
