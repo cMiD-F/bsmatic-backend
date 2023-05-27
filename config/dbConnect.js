d7c9e9365ec8924daf24ef8eece22e7ccd6b6136
cMiD-F <cauet.cmd@gmail.com>
2023-05-27 12:18:20 -0300

    v0.2

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const {default: mongoose} = require("mongoose");
 const dbConnect = () => {
     try {
         const conn = mongoose.connect(process.env.MONGODB_URL);
-        console.log("Conexão com o banco de dados realizada com sucesso");
+        console.log("Conexão com o banco de dados realizada com sucesso!");
     } catch (error) {
         console.log("Erro no banco de dados");
     }
